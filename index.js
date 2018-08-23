664f0fafe4e3a82253b36fe8c17281d6b0241e23
MaricruzJ <maricruzj.er@gmail.com>
2018-08-23 18:06:53 -0500

    entrar

--- index.js
+++ index.js
@@ -1,42 +1,64 @@
 #!/usr/bin/env node
-
 const [, , ...args] = process.argv
-console.log(`hello world! ${args}`)
+console.log(`${args}`)
 
 const regex = new RegExp('^(?:[a-z]+:)?//', 'i');
 const abs = 'Es una url absoluta';
 const rel = 'Es una ruta relativa';
+const path = require('path');
+const fs = require('fs');
 
 
 const validarUrl = (urlString) => {
-    if (regex.test(urlString) === true) {
+    //console.log(`${urlString}`);
+    /*  if (path.isAbsolute(`'${urlString}'`)) {
+         console.log('Es una ruta absoluta');
+         return abs;
+     } */
+    if (regex.test(urlString)) {
         console.log('Es una ruta absoluta');
+        //directoryOrFile(urlString);
         return abs;
+    } else {
+        console.log('Es una ruta relativa');
+        return rel;
     }
-    console.log('Es una ruta relativa');
-    return rel;
-}
-module.exports = validarUrl;
-
-
-const path = (url) => {
-    // validar si es relativo o absoluto
-    // ver regex 
     // cuando sea relativo, => hacer que sea absoluto resolve()  o isabsolute()
     // retorna url absoluta
 }
-
-const fileOrDirectory = () => {
-    // validar si es directorio o file
-    // si es directorio recorrer hasta encontrar files => 
-    // filtrar si es markdown => extname() ver basename() ver format() fs.stat()
-    // paso previo
-    // retorna archivos markdown filtrados
-}
-
-const mdLinks = (path, options) => {
-
-}
-
+module.exports = validarUrl;
 
 validarUrl(args);
+
+const directoryOrFile = (url) => {
+    //fs.stat objeto que proporciona información sobre un archivo.
+    fs.stat(url, (err, stats) => {
+
+        if (stats.isFile()) {
+            console.log('archivo');
+            // Aquí se obtendrán los que son md
+            //getLinks();
+        } else if (stats.isDirectory()) {
+            console.log('directorio');
+            // Lee el contenido de un directorio
+            fs.readdir(stats, (err, files) => {
+                // Aquí con de entre los archivos se obtendrán los archivos que tengan extensión md, luego los paso a getLinks();
+                //getLinks();
+            });
+        }
+    });
+
+    // si es directorio recorrer hasta encontrar files => 
+    // filtrar si es markdown 
+    // retorna archivos markdown filtrados
+};
+
+const getLinks = () => {
+    // Leer el archivo
+    // filtrar los links
+}
+
+
+/* const mdLinks = (path, options) => {
+
+} */
